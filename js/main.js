38bd72bd73a7426e2c7c846ca0d2a430fbcc07bd
capoaira <capoaira@web.de>
2020-04-06 19:54:46 +0200

    Entferne index.html (#27)
    Entfernt alles was nicht mit dem Konfigurator zu tun hat, weil Website über Jimdo laufen muss

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$(".menue-btn").click(function() {
-		if ($(".menue-list").hasClass("open")) {
-			$(".menue-list").removeClass("open");
-		} else {
-			$(".menue-list").addClass("open");
-		}
-	});
-});
